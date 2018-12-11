2afb96eb2efc42e2f753af1e8666dd44663e38c8
lacey <hiu@example.com>
2018-12-11 20:47:59 +0000

    finish extensions

--- controller.rb
+++ controller.rb
@@ -5,7 +5,23 @@ require_relative 'models/game'
 also_reload './models/*'
 
 
+# FINISH MVP
 
 get '/:hand1/:hand2' do
   return "#{Game.check_win(params[:hand1], params[:hand2])}"
+end
+
+#EXTENSION
+
+get '/' do
+  erb(:home)
+end
+
+get '/about' do
+  erb(:about)
+end
+
+get '/:hand1/:hand2' do
+  @winner = Game.check_win(params[:hand1], params[:hand2])
+  erb(:result)
 end
